3a5af358735b617474d455540b2fc591d380f73b
RAJESH KUMAR ATTRI <nitin@RAJESHs-MacBook-Air.local>
2026-04-12 14:13:26 +0530

    Updated code

--- App.js
+++ App.js
@@ -6,14 +6,18 @@
  * </div>
  *
  */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h1", {}, "I'm an h2 tag"),
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "h2-1" }, "I'm an h2 tag"),
   ]),
-);
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "h2-2" }, "I'm an h2 tag"),
+  ]),
+]);
 
 // const heading = React.createElement(
 //   "h1",
